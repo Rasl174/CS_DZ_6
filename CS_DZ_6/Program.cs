7328ae29ea9bc1e32d99b6242f7050b5834518ad
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-08 12:04:52 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 10;
+            string exit = "exit";
+            string userInput;
+
+            for(int i = 0; i < tryCount; i++)
+            {
+                Console.Write("Введите exit для выхода: ");
+                userInput = Console.ReadLine();
+
+                if(userInput == exit)
+                {
+                    Console.WriteLine("Программа закрыта.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Программа выполнится еще " + (tryCount - i) + " раз");
+                }
+            }
+        }
+    }
+}
